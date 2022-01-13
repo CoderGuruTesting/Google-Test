9acdc8321fb2c6df04df153ddc376c522bc4dd89
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-01-13 19:30:41 +0000

    fix html auditing bug

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ function onSignIn(googleUser) {
 
     document.getElementById("userImg").src = profile.getImageUrl();
 
-    document.querySelector(".name") = profile.getName();
-    document.querySelector(".email") = profile.getEmail();
+    document.querySelector(".name").innerHTML = profile.getName();
+    document.querySelector(".email").innerHTML = profile.getEmail();
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-      console.log('User signed out.');
+        document.getElementById("userImg").src = "";
+
+        document.querySelector(".name").innerHTML = "";
+        document.querySelector(".email").innerHTML = "";
     });
 }
 
@@ -29,6 +32,6 @@ if (auth2.isSignedIn.get()) {
 
     document.getElementById("userImg").src = profile.getImageUrl();
 
-    document.querySelector(".name") = profile.getName();
-    document.querySelector(".email") = profile.getEmail();
+    document.querySelector(".name").innerHTML = profile.getName();
+    document.querySelector(".email").innerHTML = profile.getEmail();
 }
