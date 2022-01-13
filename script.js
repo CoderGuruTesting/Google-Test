1b7c4ffea889e56212dca6a37db47222c4b7ab25
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-01-13 19:26:05 +0000

    Load on login

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function signOut() {
       console.log('User signed out.');
     });
 }
+
+gapi.auth2.init();
+
+if (auth2.isSignedIn.get()) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+
+    document.getElementById("userImg").src = profile.getImageUrl();
+
+    document.querySelector(".name") = profile.getName();
+    document.querySelector(".email") = profile.getEmail();
+}
