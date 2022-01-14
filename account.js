a2b86d5949fae2cb8e06553117fab6dff64c6a18
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-01-14 13:40:00 +0000

    move everything to account.js

--- account.js
+++ account.js
@@ -40,18 +40,35 @@ function signOut() {
 //     }
 // }
 
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.6.3/firebase-database.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+import {
+    getAnalytics
+} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-analytics.js";
 
-const database = firebase.database();
+const firebaseConfig = {
+    apiKey: "AIzaSyDGyliCvD8UjUOAphjguLLuH55E-Y4r9Uo",
+    authDomain: "turnkey-agility-338113.firebaseapp.com",
+    databaseURL: "https://turnkey-agility-338113-default-rtdb.firebaseio.com",
+    projectId: "turnkey-agility-338113",
+    storageBucket: "turnkey-agility-338113.appspot.com",
+    messagingSenderId: "796393754460",
+    appId: "1:796393754460:web:ab34cf0f23a5bda3acbb99",
+    measurementId: "G-R75G7P7N72"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function writeUserData(userId, name, email, imageUrl) {
     firebase.database().ref('users/' + userId).set({
         username: name,
         email: email,
-        profile_picture : imageUrl
+        profile_picture: imageUrl
     });
 }
 
-document.getElementById("dataTest").onclick = function() {
+document.getElementById("dataTest").onclick = function () {
     writeUserData("TESTID", "NAME", "example@gmail.com", "test.jpg");
 };
