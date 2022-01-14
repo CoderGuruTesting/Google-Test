7208acd739d2684dd37fc50c565ffe88a945e2c7
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-01-14 13:40:42 +0000

    comment everything

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-import {
-  initializeApp
-} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
-import {
-  getAnalytics
-} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-analytics.js";
+// import {
+ // initializeApp
+//} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js";
+//import {
+  //getAnalytics
+//} from "https://www.gstatic.com/firebasejs/9.6.3/firebase-analytics.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDGyliCvD8UjUOAphjguLLuH55E-Y4r9Uo",
-  authDomain: "turnkey-agility-338113.firebaseapp.com",
-  databaseURL: "https://turnkey-agility-338113-default-rtdb.firebaseio.com",
-  projectId: "turnkey-agility-338113",
-  storageBucket: "turnkey-agility-338113.appspot.com",
-  messagingSenderId: "796393754460",
-  appId: "1:796393754460:web:ab34cf0f23a5bda3acbb99",
-  measurementId: "G-R75G7P7N72"
-};
+//const firebaseConfig = {
+  //apiKey: "AIzaSyDGyliCvD8UjUOAphjguLLuH55E-Y4r9Uo",
+ // authDomain: "turnkey-agility-338113.firebaseapp.com",
+  //databaseURL: "https://turnkey-agility-338113-default-rtdb.firebaseio.com",
+  //projectId: "turnkey-agility-338113",
+ // storageBucket: "turnkey-agility-338113.appspot.com",
+//  messagingSenderId: "796393754460",
+ // appId: "1:796393754460:web:ab34cf0f23a5bda3acbb99",
+ // measurementId: "G-R75G7P7N72"
+//};
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+//const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
